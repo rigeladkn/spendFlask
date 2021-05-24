bbc52fabe34cef794334bb5512b94cc74eb180b0
rigel <rigeladikan@gmail.com>
2021-05-24 12:27:04 +0100

    formulaires et listes dépenses, revenus, budgets dispos

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,18 @@ from jinja2 import TemplateNotFound
 def index():
     return render_template('index.htm')
 
+# page des dépenses
+@app.route('/depenses')
+def depenses():
+    return render_template('views/admin/depenses.htm')
+
+# page des budgets
+@app.route('/budgets')
+def budgets():
+    return render_template('views/admin/budgets.htm')
+
+
+
 # Outil pour tester les routes
 with app.test_request_context():
     print(url_for('index'))
